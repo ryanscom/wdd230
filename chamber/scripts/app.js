af4dc42c22574184e119d17f96826b859a7e6412
ryanscom <ryanscom@byui.edu>
2023-01-26 11:55:56 -0800

    Large body outline and extras.

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,24 +1,36 @@
-//Toggle menu icon****************************************************
+
+//Toggle menu icon**************************************************************
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
-    // console.log("It Worked!!!");
 }
 
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
 
-// header Date ********************************************************
+// header Date *****************************************************************
+const headerdate = document.querySelector("#headerdate"); 
 
-const headerdate = document.querySelector("#headerdate");
-headerdate.innerHTML = new Date().getFullYear();
+const now = Date.now(); 
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 
-// Footer Date ********************************************************
+headerdate.innerHTML = `Today is ${fulldate}`;
+
+
+// Footer Date *****************************************************************
 const date1 = document.querySelector("#date1");
 date1.innerHTML = new Date().getFullYear();
 
-// Getting the last modified date
 const date2 = document.querySelector("#date2");
 date2.innerHTML = new Date(document.lastModified).toLocaleDateString("en-US");
+
+
+
+
+
+
+
+
+
 
 
