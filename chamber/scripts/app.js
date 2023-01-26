4dea3a6e2728a779b18ed44f073b51ce40db2147
ryanscom <ryanscom@byui.edu>
2023-01-25 16:37:28 -0800

    header and footer changes

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -1,4 +1,4 @@
-//Toggle menu icon
+//Toggle menu icon****************************************************
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -8,10 +8,17 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
 
-// Date JS
+// header Date ********************************************************
+
+const headerdate = document.querySelector("#headerdate");
+headerdate.innerHTML = new Date().getFullYear();
+
+// Footer Date ********************************************************
 const date1 = document.querySelector("#date1");
 date1.innerHTML = new Date().getFullYear();
 
 // Getting the last modified date
 const date2 = document.querySelector("#date2");
 date2.innerHTML = new Date(document.lastModified).toLocaleDateString("en-US");
+
+
