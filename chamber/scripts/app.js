9856e870e0d29fea542bd187d8904e9afc02d164
ryanscom <ryanscom@byui.edu>
2023-01-28 09:14:41 -0800

    Added effects to icons and logo for fun!!!

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -26,9 +26,17 @@ date2.innerHTML = new Date(document.lastModified).toLocaleDateString("en-US");
 
 
 
+function responsiveMenu(){
+    const icon = document.querySelector('.icon');
+    const smallHead = document.querySelector('.smallHead')
+    const smallMenu = document.querySelector('.smallMenu')
+    icon.addEventListener('click', () => {
+        smallHead.classList.toggle('displayNone')
+        smallMenu.classList.toggle('displayNone');
+    })
+}
 
-
-
+responsiveMenu()
 
 
 
