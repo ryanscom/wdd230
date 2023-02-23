6113577c620eb2b9aff1f41fcb76cd5bed4c8f50
ryanscom <ryanscom@byui.edu>
2023-02-23 15:29:56 -0800

    Catching UP!

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -42,3 +42,52 @@ const closeBtn = document.querySelector("#closeBtn");
 closeBtn.addEventListener('click', () => {
     ad.remove(ad);
 });
+
+
+// Image loading ****************************************************************
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src")
+    };
+};
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach(img => {
+        observer.observe(img)
+    });
+}else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    })
+}
+
+//Number of visits **************************************************************
+
+const numVisitsDisplay = document.querySelector('#numberVisits');
+const numVisits = Number(window.localStorage.getItem('visitsLocStorage'));
+
+if (numVisits !== 0) {
+    numVisitsDisplay.textContent = numVisits;
+
+}else {
+    numVisitsDisplay.textContent = `This is your first visit!`
+}
+numVisits++;
+localStorage.setItem('visitsLocStorage', numVisits);
