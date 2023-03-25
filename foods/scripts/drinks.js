9a2c14781419bee3921e076d36e3be2c6b224c41
ryanscom <ryanscom@byui.edu>
2023-03-25 14:53:53 -0700

    freash page update

--- foods/scripts/drinks.js
+++ foods/scripts/drinks.js
@@ -211,15 +211,38 @@ const scrollToBottom = function() {
 }
 
 
-
-
-
 // Get the current user's specialty drink count from local storage or initialize it to 0
 let specialtyDrinkCount = localStorage.getItem('specialtyDrinkCount') || 0;
+
+// Get the parent node where the child node will be added
+const drinksMade = document.getElementById('drinksMadeSection');
+
+// Display the count on the information card
+const drinkCard = document.createElement('p');
+
+// drinkCard.classList.add('info-card');
+drinkCard.innerHTML = `You have made ${specialtyDrinkCount} specialty drinks!`;
+
+// Add the information card to the page
+drinksMade.appendChild(drinkCard);
+
+
 
 // Update the count when the form is submitted
 const form = document.querySelector('form');
 form.addEventListener('submit', () => {
-  specialtyDrinkCount++;
-  localStorage.setItem('specialtyDrinkCount', specialtyDrinkCount);
+    specialtyDrinkCount++;
+    localStorage.setItem('specialtyDrinkCount', specialtyDrinkCount);
+
+    // drinkCard.classList.add('info-card');
+    drinkCard.innerHTML = `You have made ${specialtyDrinkCount} specialty drinks!`;
+
+    // Add the information card to the page
+    drinksMade.appendChild(drinkCard);
 });
+
+
+
+
+
+
